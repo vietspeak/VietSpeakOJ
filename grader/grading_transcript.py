d7167472ded0c6612801d461b0380c423bb37207
dvbui <dvbui@wisc.edu>
2022-10-15 20:47:05 -0500

    simple grader

--- grader/grading_transcript.py
+++ grader/grading_transcript.py
@@ -1,4 +1,5 @@
 from typing import List
+from model.model import CMUPronunciation
 
 from utils.dictionary import Dictionary
 from utils.lcs import LongestCommonSubsequence
@@ -27,3 +28,33 @@ class GradingTranscript:
             sample_pronounce, audio_pronounce
         )
         return " ".join(longest_common)
+
+class SimpleGrader:
+    def __init__(self, dictionary: Dictionary):
+        self.dictionary = dictionary
+    
+    @classmethod
+    def _get_chosen_arpabet(cls, pronunciations: List[CMUPronunciation]) -> CMUPronunciation:
+        max_schwa = 0
+        chosen = None
+        for pronounce in pronunciations:
+            number_of_schwa = pronounce.arpabet.count("AH")
+            if chosen is None or number_of_schwa > max_schwa:
+                max_schwa = number_of_schwa
+                chosen = pronounce
+        return chosen
+
+    def _get_chosen_arpabet_script(self, script: str) -> List[str]:
+        pronunciations = self.dictionary.get_pronunciation_from_text(script)
+        arpabets = [self._get_chosen_arpabet(pronounce).arpabet.split() for pronounce in pronunciations]
+        result = []
+        for arpa in arpabets:
+            result += arpa
+        return result
+
+    
+    def grader(self, student_script: str, grading_script: str) -> float:
+        student_arpabet = self._get_chosen_arpabet_script(student_script)
+        grading_arpabet = self._get_chosen_arpabet_script(grading_script)
+        common = LongestCommonSubsequence.solve(student_arpabet, grading_arpabet)
+        return len(common) / max(1, len(grading_arpabet))
