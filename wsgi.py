8aca06cba36f47de50316bf554b4f0e21e4bd2f9
Bùi Việt Dũng <47425250+dvbui@users.noreply.github.com>
2022-10-23 14:31:18 -0500

    19 proper deployment instructions (#20)
    * update gitignore
    
    * deployment instructions

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 import os
-
-from slack.app import app
+from slack.app import flask_app
 
 if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+    flask_app.run(port=int(os.environ.get("PORT", 3000)))
