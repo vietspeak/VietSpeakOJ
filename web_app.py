87124056cfe126b3dbf752c2ed20e55ab408d253
dvbui <dvbui@wisc.edu>
2022-10-29 17:43:38 -0500

    add host

--- web_app.py
+++ web_app.py
@@ -2,4 +2,4 @@ from website.app import app
 import os
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.environ.get("ADMIN_PORT", 5000))
+    app.run(host='0.0.0.0', debug=True, port=os.environ.get("ADMIN_PORT", 5000))
