65eb7bbe431bbea042bb5efc0a19973b23046043
dvbui <dvbui@wisc.edu>
2022-10-29 13:44:09 -0500

    try link only

--- tests/test_vtt.py
+++ tests/test_vtt.py
@@ -1,5 +1,5 @@
 from bridges.submission_audio_bridge import vtt_link_to_transcript
 
 def test_vtt():
-    result = vtt_link_to_transcript("https://files.slack.com/files-tmb/T01BUG1HQDB-F048NQEEXTM-9cef57b246/file.vtt?_xcb=b0435")
+    result = vtt_link_to_transcript("https://files.slack.com/files-tmb/T01BUG1HQDB-F048NQEEXTM-9cef57b246/file.vtt")
     assert "An english professor wrote the words Woman without her man" in result
