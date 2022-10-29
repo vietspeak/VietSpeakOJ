82c0b374b5b82d902a9133511ed6ad92daea1517
dvbui <dvbui@wisc.edu>
2022-10-29 17:56:58 -0500

    infinite loop

--- runner.py
+++ runner.py
@@ -14,13 +14,15 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
 )
 
-with Session(engine) as session:
-    dictionary = Dictionary(session)
-    grading_transcript_producer = GradingTranscript(dictionary)
-    task_determiner = TaskDeterminer(session)
-    grader = LegacyGrader(dictionary)
-    slack_user_bridge.entry_point(session)
-    task_audio_bridge.entry_point(session, grading_transcript_producer)
-    submission_audio_bridge.entry_point(session)
-    submission_transcript_bridge.entry_point(session, task_determiner)
-    submission_task_bridge.entry_point(app, session, grader)
+while True:
+    with Session(engine) as session:
+        dictionary = Dictionary(session)
+        grading_transcript_producer = GradingTranscript(dictionary)
+        task_determiner = TaskDeterminer(session)
+        grader = LegacyGrader(dictionary)
+        slack_user_bridge.entry_point(session)
+        task_audio_bridge.entry_point(session, grading_transcript_producer)
+        submission_audio_bridge.entry_point(session)
+        submission_transcript_bridge.entry_point(session, task_determiner)
+        submission_task_bridge.entry_point(app, session, grader)
+        time.sleep(5)
