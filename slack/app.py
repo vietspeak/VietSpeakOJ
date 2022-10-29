9bad857bc641fa4fe683013c014a22ad3f12361a
dvbui <dvbui@wisc.edu>
2022-10-29 16:21:30 -0500

    use real user id

--- slack/app.py
+++ slack/app.py
@@ -3,12 +3,13 @@ from typing import Any, Dict, Optional
 import logging
 
 # Use the package we installed
-from slack_bolt import App
+from slack_bolt import App, Say
 from slack_bolt.adapter.flask import SlackRequestHandler
 from config.config import MANDATORY_CHANNEL
-from model.model import FileSource, Submission, engine
+from model.model import FileSource, Submission, User, engine
 from slack_sdk.web.client import WebClient
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 # Initializes your app with your bot token and signing secret
 app = App(
@@ -18,12 +19,18 @@ app = App(
 
 
 @app.event("file_shared")
-def file_shared_handler(event: Optional[Dict[str, Any]]):
-    
+def file_shared_handler(event: Optional[Dict[str, Any]], say: Say):
     with Session(engine) as session:
+        find_real_user_id = select(User).where(User.slack_id == event.get("user_id"))
+        user: User = next(session.scalars(find_real_user_id), None)
+
+        if not user:
+            say("Bot chưa biết bạn là ai. Hãy đợi bot 1 phút để tìm hiểu bạn.")
+            return
+        
         new_submission = Submission(
             source=FileSource.SLACK,
-            user_id=event.get("user_id"),
+            user_id=user.id,
             audio_file=bytes(event.get("file_id"), encoding="utf-8"),
         )
         session.add(new_submission)
