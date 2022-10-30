5fb44bd28ee8bd7b0fc3f74a322e6531c4ac807f
dvbui <dvbui@wisc.edu>
2022-10-30 09:32:19 -0500

    ready for testing

--- bridges/slack_user_bridge.py
+++ bridges/slack_user_bridge.py
@@ -2,23 +2,19 @@ import logging
 import random
 import string
 import time
-from typing import List
+from typing import Any, Dict, List
 
 from slack_sdk.errors import SlackApiError
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from model.model import User
-from slack.app import app
+from slack_bolt import App
 
 logger = logging.getLogger(__name__)
 
 
-def generate_password(length: int = 10) -> str:
-    return "".join(random.choice(string.ascii_letters) for _ in range(length))
-
-
-def entry_point(session: Session = None):
+def entry_point(app: App, session: Session = None):
     all_users = []
     try:
         cursor = None
@@ -36,28 +32,12 @@ def entry_point(session: Session = None):
 
     new_users: List[User] = []
     for user in all_users:
-        print(user)
         find_user_stmt = select(User).where(User.slack_id == user["id"])
         user_obj: User = next(session.scalars(find_user_stmt), None)
-
-        user_email = user["profile"].get("email")
-        is_bot = user.get("is_bot", False)
-        is_owner = user.get("is_owner", False)
-        is_admin = user.get("is_admin", False)
         if user_obj:
-            user_obj.email = user_email
-            user_obj.is_bot = is_bot
-            user_obj.is_owner = is_owner
-            user_obj.is_admin = is_admin
+            user_obj.update_from_dict(user)
         else:
-            user_obj = User(
-                slack_id=user["id"],
-                email=user_email,
-                password=generate_password(),
-                is_bot=is_bot,
-                is_owner=is_owner,
-                is_admin=is_admin,
-            )
+            user_obj = User.from_dict(user)
             new_users.append(user_obj)
 
     session.add_all(new_users)
