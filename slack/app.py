dfb3de4c6434ff5304ca747ee9f293a41e6aa092
dvbui <dvbui@wisc.edu>
2022-11-26 19:51:54 -0600

    try filter again

--- slack/app.py
+++ slack/app.py
@@ -85,7 +85,10 @@ def file_shared_handler(event: Optional[Dict[str, Any]], say: Say):
         session.add(new_submission)
         session.commit()
 
-@app.message()
+@app.event({
+    "type": "message",
+    "subtype": "message_replied"
+})
 def a_likely_feedback_is_posted(logger, event: Optional[Dict[str, Any]]):
     user, text = event["user"], event["text"]
     logger.info(f"The user {user} changed the message to {text}")
