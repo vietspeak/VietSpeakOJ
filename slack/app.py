8ed849735be405dae19e3d8802679a022c0400d6
dvbui <dvbui@wisc.edu>
2022-11-26 19:53:23 -0600

    subtype does not work?

--- slack/app.py
+++ slack/app.py
@@ -86,8 +86,7 @@ def file_shared_handler(event: Optional[Dict[str, Any]], say: Say):
         session.commit()
 
 @app.event({
-    "type": "message",
-    "subtype": "message_replied"
+    "type": "message"
 })
 def a_likely_feedback_is_posted(logger, event: Optional[Dict[str, Any]]):
     user, text = event["user"], event["text"]
