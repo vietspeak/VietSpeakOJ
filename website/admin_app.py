0bc2e42f58509ea629f201c7cc39d6755c356b77
dvbui <dvbui@wisc.edu>
2022-12-22 19:26:44 -0600

    task edit for admin only

--- website/admin_app.py
+++ website/admin_app.py
@@ -2,7 +2,7 @@ import io
 
 from flask import Flask, flash, redirect, render_template, request
 from sqlalchemy.orm import Session
-
+from sqlalchemy import select, desc
 from model.model import FileSource, Submission, Task, TaskLevel, engine
 
 app = Flask(__name__, template_folder="templates")
@@ -68,3 +68,40 @@ def test_task():
                 session.commit()
 
     return render_template("test_task.html")
+
+@app.route("/edit_tasks", methods=["GET", "POST"])
+def edit_tasks():
+    if request.method == "POST":
+        task_id = int(request.values.get("task_id", 0))
+        with Session(engine) as session:
+            stmt = select(Task).where(Task.id == task_id)
+            task = session.scalar(stmt)
+            if task:
+                task.task_number = int(request.values.get("task_number", 0))
+                task.level = TaskLevel._member_map_[request.values.get("task_level")]
+                task.title = request.values.get("task_title", "")
+                task.sample_transcript = request.values.get("transcript", "")
+            session.commit()
+        
+        return "Done!"
+
+            
+    result = ["<html>"]
+    with Session(engine) as session:
+        stmt = select(Task).order_by(desc(Task.task_number))
+        
+        for task in session.scalars(stmt):
+            task: Task
+            result.append(f"""
+                <form id="form{task.id}" action="/edit_tasks" method="post">
+                    ID: <input type="text" name="task_id" value="{task.id}"></input><br>
+                    Task number: <input type="text" name="task_number" value="{task.task_number if task.task_number is not None else ''}"></input><br>
+                    Level: <input type="text" name="task_level" value="{str(task.level).split(".")[1]}"></input><br>
+                    Title: <input type="text" name="task_title" value="{task.title if task.title is not None else ''}"></input><br>
+                    Sample transcript: <textarea name="transcript" form="form{task.id}">{task.sample_transcript if task.sample_transcript is not None else ''}</textarea><br>
+                    <input type="submit" value="Edit Task ID {task.id}"></input>
+                </form>
+            """)
+
+    result.append("</html>")
+    return "".join(result)
