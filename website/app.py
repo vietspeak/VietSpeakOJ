e632f9752f56d704574558b577eadefca942afed
Bùi Việt Dũng <47425250+dvbui@users.noreply.github.com>
2022-12-24 14:14:16 -0600

    create a simple task view (#65)

--- website/app.py
+++ website/app.py
@@ -3,8 +3,8 @@ import io
 from typing import List
 
 import pytz
-from flask import Flask, render_template
-from sqlalchemy import desc, select
+from flask import Flask, render_template, request
+from sqlalchemy import desc, select, and_
 from sqlalchemy.orm import Session
 
 from config.config import MAX_NUMBER_OF_SUBMISSIONS_IN_QUEUE
@@ -21,11 +21,79 @@ COLOR_MAP = {
 }
 
 
-@app.route("/submission_queue")
+def get_max_task_number():
+    with Session(engine) as session:
+        stmt = """
+            SELECT MAX(task_number)
+            FROM tasks;
+        """
+
+        result = next(session.execute(stmt))
+        return result[0]
+
+
+def check_if_task_is_available(task_number):
+    with Session(engine) as session:
+        stmt = select(Task).where(Task.task_number == task_number)
+        result = next(session.scalars(stmt), None)
+        return result is not None
+
+
+@app.route("/")
+def home_page():
+    return (
+        render_template("header.html")
+        + render_template("home_body.html")
+        + render_template("footer.html")
+    )
+
+
+@app.route("/tasks", methods=["GET"])
+def tasks_page():
+    task_number = int(request.values.get("number", get_max_task_number()))
+
+    previous_task_link = (
+        f"""
+        <a href="/tasks?number={task_number-1}">&#8592; Task {task_number-1}</a>
+    """
+        if check_if_task_is_available(task_number - 1)
+        else ""
+    )
+
+    next_task_link = (
+        f"""
+        <a href="/tasks?number={task_number+1}">Task {task_number+1} &rarr;</a>
+    """
+        if check_if_task_is_available(task_number + 1)
+        else ""
+    )
+
+    task_level_str = request.values.get("level", "Yellow")
+    task_level = TaskLevel._member_map_.get(task_level_str.upper(), TaskLevel.YELLOW)
+
+    with Session(engine) as session:
+        task_stmt = select(Task).where(and_(Task.task_number == task_number,Task.level == task_level))
+        task_info: Task = session.scalar(task_stmt)
+        print(task_info)
+        return (
+            render_template("header.html")
+            + render_template(
+                "tasks_body.html",
+                task_number=str(task_number),
+                previous_task_link=previous_task_link,
+                next_task_link=next_task_link,
+                task_level=task_level_str,
+                task_link="",
+                task_transcript=task_info.sample_transcript if task_info else ""
+            ) + render_template("footer.html")
+        )
+
+
+@app.route("/status")
 def submission_queue():
     result: List[str] = [
         """
-        <h1>Submission Queue</h1>
+        <div class="container"><h1>Status</h1>
         <table class="table">
             <tr>
                 <th>#</th>
@@ -74,6 +142,6 @@ def submission_queue():
             """
             )
 
-    result.append("</table>")
+    result.append("</table></div>")
     html_source = "\n".join(result)
     return render_template("header.html") + html_source + render_template("footer.html")
