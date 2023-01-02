407898df7da54d07d4f8d2d0e6f89979f7d2abf7
dvbui <dvbui@wisc.edu>
2023-01-02 01:13:28 -0600

    update rate limiter

--- website/app.py
+++ website/app.py
@@ -415,8 +415,8 @@ LIST_OF_SOUNDS = [
 
 
 @app.route("/profile")
-@login_required
 @limiter.limit("1 per minute")
+@login_required
 def profile():
     with Session(engine) as session:
         submissions_stmt = f"""
