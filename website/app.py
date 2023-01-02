6f3f7dbe89fdf48651117147a48d0a5ec5e10612
dvbui <dvbui@wisc.edu>
2023-01-02 01:07:30 -0600

    test speed

--- website/app.py
+++ website/app.py
@@ -82,7 +82,7 @@ def calculate_progress(session: Session, user_id: int, sound: str) -> float:
         FROM pronunciation_matches P, submissions S
         WHERE S.user_id = {user_id} AND S.is_official AND S.id = P.submission_id AND P.grading_sound='{sound}'
         ORDER BY P.id DESC
-        LIMIT 1000;
+        LIMIT 100;
     """
 
     results = list(session.execute(stmt))
